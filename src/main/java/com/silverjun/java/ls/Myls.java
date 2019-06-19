898d7c0686efaa482a36c0164979cc437f878fe5
SilverJun <jangej1031@naver.com>
2019-06-19 13:22:58 +0900

    Add annotations.

--- src/main/java/com/silverjun/java/ls/Myls.java
+++ src/main/java/com/silverjun/java/ls/Myls.java
@@ -52,9 +52,9 @@ public class Myls {
 			islOption = cmd.hasOption("l");
 			ishOption = cmd.hasOption("h");
 			
-			File pwd = new File(System.getProperty("user.dir"));
+			File pwd = new File(System.getProperty("user.dir"));		// get current working directory.
 			
-			if (cmd.getArgList().size() > 0)
+			if (cmd.getArgList().size() > 0)		// 만약 추가적으로 디렉터리를 입력했으면 그 디렉터리로 설정.
 				pwd = new File(cmd.getArgList().get(0));
 			
 			printFileDirectory(pwd);
@@ -69,6 +69,11 @@ public class Myls {
 		}
 	}
 
+	/**
+	 * print one file method
+	 * @param file to print.
+	 * @throws IOException
+	 */
 	private void printOneFile(File file) throws IOException
 	{
 		if (islOption)
@@ -86,18 +91,22 @@ public class Myls {
 			String groupName = posixAttr.group().getName();
 			String permission = PosixFilePermissions.toString(posixAttr.permissions());
 			
+			// get file size
 			long fileSize = (long)Files.getAttribute(path, "basic:size", NOFOLLOW_LINKS);
 			String fileSizeStr = String.valueOf(fileSize);
-			if (ishOption)
+			if (ishOption)		// human readable option is on, change file size.
 			{
 				fileSizeStr = readableFileSize(fileSize);
 			}
 			
+			// get file last modified time
 			FileTime fileTime = (FileTime)Files.getAttribute(path, "basic:lastModifiedTime", NOFOLLOW_LINKS);
-			
-			int linkCount = (int)Files.getAttribute(path, "unix:nlink", NOFOLLOW_LINKS);
+			// formatting.
 			SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd HH:mm yyyy", Locale.US);
 			String lastFixedTime = dateFormat.format(new Date(fileTime.toMillis()));
+			
+			// get unix link count.
+			int linkCount = (int)Files.getAttribute(path, "unix:nlink", NOFOLLOW_LINKS);
 			
 			System.out.print(String.format("%s%s%4d %10s %6s %7s %s %s", type, permission, linkCount, ownerName, groupName, fileSizeStr, lastFixedTime, file.getName()));
 		}
@@ -111,6 +120,7 @@ public class Myls {
 			else if (file.isFile())	System.out.print("*");
 		}
 		
+		// resolve link file.
 		if (Files.isSymbolicLink(file.toPath()))
 			System.out.print(" -> " + file.toPath().toRealPath().getFileName());
 		
@@ -123,7 +133,7 @@ public class Myls {
 		// 1. get file list
 		if (isaOption)
 			fileList.addAll(Arrays.asList(directory.listFiles()));
-		else
+		else	// get only visible files.
 			fileList.addAll(Arrays.asList(directory.listFiles((FileFilter)HiddenFileFilter.VISIBLE)));
 		
 		// 2. sort if there is no f option.
@@ -145,7 +155,7 @@ public class Myls {
 		}
 		if (isaOption)
 		{
-			// . and .. print
+			// add . and ..
 			fileList.add(0, new File(".."));
 			fileList.add(0, new File("."));
 		}
